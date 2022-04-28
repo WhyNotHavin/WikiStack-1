4deb9ae1f3f2d1ea3bff840b32dcd684a386abb0
Miguel Cruz <rednu7@yahoo.com>
2022-04-28 17:34:24 -0400

    did what we could

--- models/index.js
+++ models/index.js
@@ -4,37 +4,35 @@ const db = new Sequelize("postgres://localhost:5432/wikistack");
 const Page = db.define("page", {
   title: {
     type: Sequelize.STRING,
-    max:64,
-    notNull:true
+    max: 64,
+    notNull: true,
   },
   slug: {
     type: Sequelize.STRING,
-    notNull:true
+    notNull: true,
   },
   content: {
     type: Sequelize.TEXT,
-    notNull:true
+    notNull: true,
   },
   status: {
     type: Sequelize.ENUM("open", "closed"),
   },
 });
 Page.beforeCreate((page) => {
-  page.slug = page.title.split(' ').join('_')
-})
-
-
+  page.slug = page.title.split(" ").join("_").toLowerCase();
+});
 
 const User = db.define("user", {
   name: {
     type: Sequelize.STRING,
-    max:32,
-    notNull:true
+    max: 32,
+    notNull: true,
   },
   email: {
     type: Sequelize.STRING,
-    notNull:true,
-    isEmail:true
+    notNull: true,
+    isEmail: true,
   },
 });
 
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,51 +1,59 @@
 const router = require("express").Router();
-const {addPage} = require('../views')
-const {main} = require('../views')
-const {wikipage} = require('../views')
-const {Page} = require("../models")
+const { addPage } = require("../views");
+const { main } = require("../views");
+const wikipage = require("../views/wikipage");
+const { Page } = require("../models");
 router.use(function (req, res, next) {
   // .. some logic here .. like any other middleware
-  next()
-})
+  next();
+});
 
-
-
-router.get('/', (req,res) =>{
+router.get("/", (req, res) => {
   try {
-    res.send('Got to GET /wiki/')
+    res.send("Got to GET /wiki/");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-
-})
-router.get('/add', (req,res) =>{
+});
+router.get("/add", (req, res) => {
   try {
-    res.send(addPage())
+    res.send(addPage());
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
-
-router.post('/', async (req,res) =>{
+router.post("/", async (req, res) => {
   try {
-      await Page.create({
-        title: req.body.title,
-        content: req.body.content
-      })
-      res.redirect('/')
+    const post = await Page.create({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    res.redirect("/wiki/" + post.slug);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
-
-router.get('/:id', (req,res) =>{
+router.get("/:slug", async (req, res, next) => {
   try {
-    res.send(main())
+    let current = await Page.findOne({
+      where: { slug: req.params.slug },
+    });
+    current = wikipage(current);
+    res.send(current);
   } catch (error) {
-    console.error(error)
+    console.log(error);
+    next();
   }
-})
+});
 
-module.exports = router
+router.get("/:id", (req, res) => {
+  try {
+    res.send(main());
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
