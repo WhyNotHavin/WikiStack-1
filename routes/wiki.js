793a6d0cc3c3e3ebf0467fc3ed701cd27eff7910
Havin Mickel <12345678900qw@gmail.com>
2022-04-28 15:00:30 -0700

    END OF DAY

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,6 +3,7 @@ const { addPage } = require("../views");
 const { main } = require("../views");
 const wikipage = require("../views/wikipage");
 const { Page } = require("../models");
+const { Op } = require("sequelize")
 router.use(function (req, res, next) {
   // .. some logic here .. like any other middleware
   next();
@@ -48,9 +49,12 @@ router.get("/:slug", async (req, res, next) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id",async (req, res) => {
   try {
-    res.send(main());
+    const pages = await Page.findAll({
+      order: [['id', 'DESC']]
+    })
+    res.send(wikipage(pages));
   } catch (error) {
     console.error(error);
   }
